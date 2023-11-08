cf71126cd6f6205b41f0f67b1d008970647e43cb
Augustsabr <augustsabr@gmail.com>
2023-11-08 16:07:21 +0100

    oppgave 9a

--- apperPlaystore/skript.js
+++ apperPlaystore/skript.js
@@ -1,32 +1,134 @@
 const dataJson = require('./googleplaystore.json');
 
-let categories = [];
-for (let i = 0; i < dataJson.length; i++) {
-  categories.push(dataJson[i].Category)
-}
-categories.sort()
 
-let categoriesNumbered = {}
-let caunts = []
-let caunt = 1;
-for (let i = 0; i < categories.length; i++) {
-  if (categories[i] === categories[i+1]) {
-    caunt++;
-  } else {
-    categoriesNumbered[categories[i]] = caunt;
-    caunts.push(caunt)
-    caunt = 1
+function objKeysToObj(obj) {
+  let categories = [];
+  for (let i = 0; i < obj.length; i++) {
+    categories.push(obj[i].Category)
   }
+  categories.sort()
+
+  let categoriesNumbered = {}
+  let caunt = 1;
+  for (let i = 0; i < categories.length; i++) {
+    if (categories[i] === categories[i+1]) {
+      caunt++;
+    } else {
+      categoriesNumbered[categories[i]] = caunt;
+      caunt = 1
+    }
+  }
+  return categoriesNumbered
+}
+let categoriesNumbered = objKeysToObj(dataJson)
+
+
+function findTopNKeysInObj(obj, n) {
+  // gjør objekt til 2d array
+  let entries = Object.entries(obj);
+
+  // sorter array
+  entries.sort(function(a, b) {
+    return b[1] - a[1];
+  });
+
+  // Fjern alle elementer bortsett fra de n første
+  if (entries.length > n) {
+    entries = entries.slice(0, n);
+  }
+  return entries
+}
+const n = 3
+const highestKeys = findTopNKeysInObj(categoriesNumbered, n)
+
+
+function averageRating(obj, category) {
+  let ratings = [];
+  for (let i = 0; i < obj.length; i++) {
+    if (obj[i].Category === category && obj[i].Rating !== 'NaN') {
+      ratings.push(obj[i].Rating)
+    }
+  }
+  console.log(ratings);
+  //gjennomsnitt
+  let sum = 0.0;
+  for(let i = 0; i < ratings.length; i++) {
+    sum += parseFloat(ratings[i]);
+  }
+  return sum / ratings.length;
 }
 
-caunts.sort(function(a, b){return a - b})
-var most = []
-for (let i = 0; i < 3; i++) {
-  most.push(caunts[caunts.length-3+i]) 
+
+function averageInstalls(obj, category) {
+  let installs = [];
+  for (let i = 0; i < obj.length; i++) {
+    if (obj[i].Category === category && obj[i].Installs !== 'NaN') {
+      installs.push(obj[i].Installs)
+    }
+  }
+
+  for (let i = 0; i < installs.length; i++) {
+    installs[i] = installs[i].replaceAll(",", "")
+    installs[i] = installs[i].replaceAll("+", "")
+  }
+
+  // console.log(installs);
+  //gjennomsnitt
+  let sum = 0.0;
+  for(let i = 0; i < installs.length; i++) {
+    sum += parseInt(installs[i]);
+  }
+  
+  return sum / installs.length;
 }
 
 
-console.log(most);
-console.log(caunts);
-// console.log(categoriesNumbered)
+// function popularApp(obj, category) {
+//   let categoryApps = {};
+//   for (let i = 0; i < obj.length; i++) {
+//     if (obj[i].Category = category) {
+//       categoryApps[obj[i].App] = obj[i].Installs;
+//     }
+//   }
+
+//   categoryApps = Object.entries(categoryApps);
+
+//   for (let i = 0; i < categoryApps.length; i++) {
+//     categoryApps[i][1] = categoryApps[i][1].replaceAll(",", "")
+//     categoryApps[i][1] = categoryApps[i][1].replaceAll("+", "")
+//   }
+
+//   // sorter array
+//   categoryApps.sort(function(a, b) {
+//     return b[1] - a[1];
+//   });
+
+//   // Fjern alle elementer bortsett fra de n første
+//   if (categoryApps.length > n) {
+//     categoryApps = categoryApps.slice(0, n);
+//   }
+//   return categoryApps
+
+// }
+// let a = popularApp(dataJson, 'GAME')
+// // console.log(a);
+
+console.log(highestKeys);
+let mostPopularCategories = [];
+for (let i = 0; i < n; i++) {
+  mostPopularCategories.push({ 'kategori': highestKeys[i][0], 'antall apper': highestKeys[i][1], 'gjennomsnittsrating': Math.round(averageRating(dataJson, highestKeys[i][0]) * 100) / 100, 'gjennomsnittlige antallet installasjoner': Math.round(averageInstalls(dataJson, highestKeys[i][0])) })
+}
+console.log();
+console.log("Mest populære kategorier:");
+console.table(mostPopularCategories);
+
+// let mostPopularApps = [];
+// for (let i = 0; i < n; i++) {
+//   for (let j = 0; j < n; j++) {
+//     mostPopularApps.push({ 'kategori': highestKeys[i][0], 'app': 'highestKeys[j][1]', 'installasjoner': 'Math.round(averageInstalls(dataJson, highestKeys[j][0]))' })
+//   }
+// }
+// console.log();
+// console.log("Mest populære apper i kategoriene:");
+// console.table(mostPopularApps);
 
