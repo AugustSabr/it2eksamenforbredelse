0b67e99f9ebbf2202e7444c6b28f8536ceac71d9
Augustsabr <augustsabr@gmail.com>
2023-11-08 20:03:48 +0100

    første fullstedige utkast

--- apperPlaystore/skript.js
+++ apperPlaystore/skript.js
@@ -49,7 +49,6 @@ function averageRating(obj, category) {
       ratings.push(obj[i].Rating)
     }
   }
-  console.log(ratings);
   //gjennomsnitt
   let sum = 0.0;
   for(let i = 0; i < ratings.length; i++) {
@@ -71,8 +70,6 @@ function averageInstalls(obj, category) {
     installs[i] = installs[i].replaceAll(",", "")
     installs[i] = installs[i].replaceAll("+", "")
   }
-
-  // console.log(installs);
   //gjennomsnitt
   let sum = 0.0;
   for(let i = 0; i < installs.length; i++) {
@@ -83,37 +80,34 @@ function averageInstalls(obj, category) {
 }
 
 
-// function popularApp(obj, category) {
-//   let categoryApps = {};
-//   for (let i = 0; i < obj.length; i++) {
-//     if (obj[i].Category = category) {
-//       categoryApps[obj[i].App] = obj[i].Installs;
-//     }
-//   }
+function popularApp(obj, category) {
+  let categoryApps = {};
+  for (let i = 0; i < obj.length; i++) {
+    if (obj[i].Category === category) {
+      categoryApps[obj[i].App] = obj[i].Installs;
+    }
+  }
 
-//   categoryApps = Object.entries(categoryApps);
+  categoryApps = Object.entries(categoryApps);
 
-//   for (let i = 0; i < categoryApps.length; i++) {
-//     categoryApps[i][1] = categoryApps[i][1].replaceAll(",", "")
-//     categoryApps[i][1] = categoryApps[i][1].replaceAll("+", "")
-//   }
+  for (let i = 0; i < categoryApps.length; i++) {
+    categoryApps[i][1] = categoryApps[i][1].replaceAll(",", "")
+    categoryApps[i][1] = categoryApps[i][1].replaceAll("+", "")
+  }
 
-//   // sorter array
-//   categoryApps.sort(function(a, b) {
-//     return b[1] - a[1];
-//   });
+  // sorter array
+  categoryApps.sort(function(a, b) {
+    return b[1] - a[1];
+  });
 
-//   // Fjern alle elementer bortsett fra de n første
-//   if (categoryApps.length > n) {
-//     categoryApps = categoryApps.slice(0, n);
-//   }
-//   return categoryApps
+  // Fjern alle elementer bortsett fra de n første
+  if (categoryApps.length > n) {
+    categoryApps = categoryApps.slice(0, n);
+  }
+  return categoryApps
+}
 
-// }
-// let a = popularApp(dataJson, 'GAME')
-// // console.log(a);
 
-console.log(highestKeys);
 let mostPopularCategories = [];
 for (let i = 0; i < n; i++) {
   mostPopularCategories.push({ 'kategori': highestKeys[i][0], 'antall apper': highestKeys[i][1], 'gjennomsnittsrating': Math.round(averageRating(dataJson, highestKeys[i][0]) * 100) / 100, 'gjennomsnittlige antallet installasjoner': Math.round(averageInstalls(dataJson, highestKeys[i][0])) })
@@ -122,13 +116,21 @@ console.log();
 console.log("Mest populære kategorier:");
 console.table(mostPopularCategories);
 
-// let mostPopularApps = [];
-// for (let i = 0; i < n; i++) {
-//   for (let j = 0; j < n; j++) {
-//     mostPopularApps.push({ 'kategori': highestKeys[i][0], 'app': 'highestKeys[j][1]', 'installasjoner': 'Math.round(averageInstalls(dataJson, highestKeys[j][0]))' })
-//   }
-// }
-// console.log();
-// console.log("Mest populære apper i kategoriene:");
-// console.table(mostPopularApps);
-
+let popularFAMILY = popularApp(dataJson, 'FAMILY')
+let popularGAME = popularApp(dataJson, 'GAME')
+let popularTOOLS = popularApp(dataJson, 'TOOLS')
+let mostPopularApps = [];
+for (let i = 0; i < n; i++) {
+  let category
+  switch (i) {
+    case 0: category = popularFAMILY; break;
+    case 1: category = popularGAME; break;
+    case 2: category = popularTOOLS; break;
+  }
+  for (let j = 0; j < n; j++) {
+    mostPopularApps.push({ 'kategori': highestKeys[i][0], 'app': category[j][0], 'installasjoner': category[j][1] })
+  }
+}
+console.log();
+console.log("Mest populære apper i kategoriene:");
+console.table(mostPopularApps);
