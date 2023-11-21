fe8dde5c7dd69158edef4422814e65f8d8be8f06
August Sabr <augustsabr@gmail.com>
2023-11-21 16:00:27 +0100

    ahaha

--- eksamen2023Vår/9ab_apperPlaystore/skript.js
+++ eksamen2023Vår/9ab_apperPlaystore/skript.js
@@ -1,10 +1,11 @@
-const dataJson = require('./googleplaystore.json');
+// jeg bruker node.js for å kjøre skriptet mitt. jeg har derfor tatt meg friheten til å bruke en av node's innebygde moduler 'require' for å hente json objektet
+const dataJson = require('./Global YouTube Statistics.json');
 
 // Lager en 2d array med kategoritypene og anntal apper innenfor katogeriene som egne arrays
 function objKeysToObj(obj) {
   let simpleCategoriesArr = [];
   for (let i = 0; i < obj.length; i++) {
-    simpleCategoriesArr.push(obj[i].Category)
+    simpleCategoriesArr.push(obj[i].category)
   }
   simpleCategoriesArr.sort()
 
@@ -43,59 +44,59 @@ const n = 3
 const highestKeys = findTopNKeysInObj(categoriesCounted, n)
 
 // finner gjennomsnittet av alle ratingene fra en valgfri ketagori
-function averageRating(obj, category) {
-  let ratings = [];
+function averageSubscribers(obj, category) {
+  let subscribers = [];
   for (let i = 0; i < obj.length; i++) {
-    if (obj[i].Category === category && obj[i].Rating !== 'NaN') {
-      ratings.push(obj[i].Rating)
+    if (obj[i].category === category) {
+      subscribers.push(obj[i].subscribers)
     }
   }
   //gjennomsnitt
   let sum = 0.0;
-  for(let i = 0; i < ratings.length; i++) {
-    sum += parseFloat(ratings[i]);
+  for(let i = 0; i < subscribers.length; i++) {
+    sum += subscribers[i];
   }
-  return sum / ratings.length;
+  return sum / subscribers.length;
 }
-
 // finner gjennomsnittet av alle installasjonene fra en valgfri ketagori
-function averageInstalls(obj, category) {
-  let installs = [];
+function averageViews(obj, category) {
+  let views = [];
   for (let i = 0; i < obj.length; i++) {
-    if (obj[i].Category === category && obj[i].Installs !== 'NaN') {
-      installs.push(obj[i].Installs)
+    if (obj[i].category === category) {
+      views.push(obj[i]["video views"])
     }
   }
 
-  for (let i = 0; i < installs.length; i++) {
-    installs[i] = installs[i].replaceAll(",", "")
-    installs[i] = installs[i].replaceAll("+", "")
-  }
+  // for (let i = 0; i < views.length; i++) {
+  //   views[i] = views[i].replaceAll(",", "")
+  //   views[i] = views[i].replaceAll("+", "")
+  // }
+
   //gjennomsnitt
   let sum = 0.0;
-  for(let i = 0; i < installs.length; i++) {
-    sum += parseInt(installs[i]);
+  for(let i = 0; i < views.length; i++) {
+    sum += views[i];
   }
   
-  return sum / installs.length;
+  return sum / views.length;
 }
 
+
 // finner de n mest populære appene innenfor en kategori
-function popularApp(obj, category, n) {
+function popularYoutuber(obj, category, n) {
   let categoryApps = {};
   for (let i = 0; i < obj.length; i++) {
-    if (obj[i].Category === category) {
-      categoryApps[obj[i].App] = obj[i].Installs;
+    if (obj[i].category === category) {
+      categoryApps[obj[i].Youtuber] = obj[i].subscribers;
     }
   }
+  categoryApps = Object.entries(categoryApps); //gjør object til 2d array
 
-  categoryApps = Object.entries(categoryApps);
-
-  for (let i = 0; i < categoryApps.length; i++) {
-    categoryApps[i][1] = categoryApps[i][1].replaceAll(",", "")
-    categoryApps[i][1] = categoryApps[i][1].replaceAll("+", "")
-    categoryApps[i][1] = parseInt(categoryApps[i][1])
-  }
+  // for (let i = 0; i < categoryApps.length; i++) {
+  //   categoryApps[i][1] = categoryApps[i][1].replaceAll(",", "")
+  //   categoryApps[i][1] = categoryApps[i][1].replaceAll("+", "")
+  //   categoryApps[i][1] = parseInt(categoryApps[i][1])
+  // }
 
   // sorter array
   categoryApps.sort(function(a, b) {
@@ -113,28 +114,28 @@ function popularApp(obj, category, n) {
 //hviser de n mest populære kategoriene
 let mostPopularCategories = [];
 for (let i = 0; i < n; i++) {
-  mostPopularCategories.push({ 'kategori': highestKeys[i][0], 'antall apper': highestKeys[i][1], 'gjennomsnittsrating': Math.round(averageRating(dataJson, highestKeys[i][0]) * 100) / 100, 'gjennomsnittlige antallet installasjoner': Math.round(averageInstalls(dataJson, highestKeys[i][0])) })
+  mostPopularCategories.push({ 'Kategori': highestKeys[i][0], 'antall kanaler': highestKeys[i][1], 'gjennomsnittlige abonenter': Math.round(averageSubscribers(dataJson, highestKeys[i][0])), 'gjennomsnittlige totale seere': Math.round(averageViews(dataJson, highestKeys[i][0])) })
 }
 console.log();
 console.log("Mest populære kategorier:");
 console.table(mostPopularCategories);
 
 //hviser de n mest populære spillene av de n mest populære kategoriene
-let popularFAMILY = popularApp(dataJson, 'FAMILY')
-let popularGAME = popularApp(dataJson, 'GAME')
-let popularTOOLS = popularApp(dataJson, 'TOOLS')
-let mostPopularApps = [];
+let popularEntertainment = popularYoutuber(dataJson, 'Entertainment')
+let popularMusic = popularYoutuber(dataJson, 'Music')
+let popularPeopleBlogs = popularYoutuber(dataJson, 'People & Blogs')
+let mostPopularYoutubers = [];
 for (let i = 0; i < n; i++) {
   let category
   switch (i) {
-    case 0: category = popularFAMILY; break;
-    case 1: category = popularGAME; break;
-    case 2: category = popularTOOLS; break;
+    case 0: category = popularEntertainment; break;
+    case 1: category = popularMusic; break;
+    case 2: category = popularPeopleBlogs; break;
   }
   for (let j = 0; j < n; j++) {
-    mostPopularApps.push({ 'kategori': highestKeys[i][0], 'app': category[j][0], 'installasjoner': category[j][1] })
+    mostPopularYoutubers.push({ 'Kategori': highestKeys[i][0], 'Youtuber': category[j][0], 'Abonenter': category[j][1] })
   }
 }
 console.log();
-console.log("Mest populære apper i kategoriene:");
-console.table(mostPopularApps);
+console.log("Mest populære Youtuber-ene i kategoriene:");
+console.table(mostPopularYoutubers);
