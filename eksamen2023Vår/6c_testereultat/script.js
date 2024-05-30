37f3ce0627c1522772d6c2beb822eba4aee39fd1
Augustsabr <augustsabr@gmail.com>
2024-05-30 09:32:00 +0200

    d

--- eksamen2023Vår/6c_testereultat/script.js
+++ eksamen2023Vår/6c_testereultat/script.js
@@ -1,7 +1,7 @@
 const testResults = [30, 65, 82, 97, 102, 0, 50, 69, 70, 89, 90, 100, -1]
 
 for (let i = 0; i < testResults.length; i++) {
-    getResultUtenFeil(testResults[i]);
+  getResultMedFeil(testResults[i]);
 }
 
 // funksjonen sjekker kun om resultatet faller mellom spesifike verdier, men ikke om resultatet er lik noen av de spesifike verdiene. F.eks er 50 et ugyldig resultat fordi det ikke er mindre en 50 eller større en 50.
@@ -21,19 +21,19 @@ function getResultMedFeil(int) {
 }
 
 function getResultUtenFeil(int) {
-    if (int < 0 || int > 100 ) {
-        console.log("Ikke gyldig poengsum!");
-    } else if (int < 50){
-        console.log("Ikke bestått");
-    } else if (int < 70){
-        console.log("Bestått");
-    } else if (int < 90){
-        console.log("Godt bestått");
-    } else if (int <= 100){
-        console.log("Meget godt bestått");
-    } else {
-        console.log("Ikke gyldig poengsum!");
-    }
+  if (int < 0 || int > 100 ) {
+    console.log("Ikke gyldig poengsum!");
+  } else if (int < 50){
+    console.log("Ikke bestått");
+  } else if (int < 70){
+    console.log("Bestått");
+  } else if (int < 90){
+    console.log("Godt bestått");
+  } else if (int <= 100){
+    console.log("Meget godt bestått");
+  } else {
+    console.log("Ikke gyldig poengsum!");
+  }
 }
 
 
